8e073f208d72b67d0cf56fc2ff37c082d2a20a00
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-15 19:03:55 +0330

    fix method CheckCredentialsRequest

--- app/src/Service/UserService.php
+++ app/src/Service/UserService.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use App\Utility\GRPC\AbstractClient;
+use Barsam\User\Messages\CheckCredentialsRequest;
 use Barsam\User\Messages\DeleteRequest;
 use Barsam\User\Messages\FieldCreateRequest;
 use Barsam\User\Messages\FieldDeleteRequest;
@@ -18,7 +19,7 @@ use Barsam\User\UserServiceInterface;
  * @method Update(UpdateRequest $request, string $responseClass, array $context = [])
  * @method Delete(DeleteRequest $request, string $responseClass, array $context = [])
  * @method Get(GetRequest $request, string $responseClass, array $context = [])
- * @method CheckCredentials(RegisterRequest $request, string $responseClass, array $context = [])
+ * @method CheckCredentials(CheckCredentialsRequest $request, string $responseClass, array $context = [])
  * @method FieldCreate(FieldCreateRequest $request, string $responseClass, array $context = [])
  * @method FieldUpdate(FieldUpdateRequest $request, string $responseClass, array $context = [])
  * @method FieldDelete(FieldDeleteRequest $request, string $responseClass, array $context = [])
