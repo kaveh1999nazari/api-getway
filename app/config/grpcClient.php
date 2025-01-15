8c6b1acabc0b70ab0c19859ae36655734cfa740a
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-15 18:26:54 +0330

    add auth Service

--- app/config/grpcClient.php
+++ app/config/grpcClient.php
@@ -5,5 +5,9 @@ return [
             'class' => \App\Service\UserService::class,
             'host' => '192.168.1.118:9001',
         ],
+        'auth' => [
+            'class' => \App\Service\AuthService::class,
+            'host' => '192.168.1.118:9002',
+        ]
     ]
 ];
