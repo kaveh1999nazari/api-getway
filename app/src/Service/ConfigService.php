053d085b3df3e07b76da3a7146acf81e548258ba
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-15 19:03:21 +0330

    create config Grpc Service

---
+++ app/src/Service/ConfigService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Service;
+
+use App\Utility\GRPC\AbstractClient;
+use Barsam\Config\ConfigServiceInterface;
+use Barsam\Config\Messages\GetOneRequest;
+use Barsam\Config\Messages\SyncRequest;
+use Barsam\Config\Messages\UpdateBatchRequest;
+use Barsam\Installment\Messages\GetAllRequest;
+
+/**
+ * @method Sync(SyncRequest $request, string $responseClass, array $context = [])
+ * @method GetOne(GetOneRequest $request, string $responseClass, array $context = [])
+ * @method GetAll(GetAllRequest $request, string $responseClass, array $context = [])
+ * @method UpdateBatch(UpdateBatchRequest $request, string $responseClass, array $context = [])
+ */
+class ConfigService extends AbstractClient
+{
+
+    public function getServiceInterface(): string
+    {
+        return ConfigServiceInterface::class;
+    }
+}
