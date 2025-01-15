e0f6a9a4e5901c4757b7d0be7888b351083ea0f7
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-15 17:47:04 +0330

    update methods

--- app/src/Service/UserService.php
+++ app/src/Service/UserService.php
@@ -4,6 +4,10 @@ namespace App\Service;
 
 use App\Utility\GRPC\AbstractClient;
 use Barsam\User\Messages\DeleteRequest;
+use Barsam\User\Messages\FieldCreateRequest;
+use Barsam\User\Messages\FieldDeleteRequest;
+use Barsam\User\Messages\FieldGetAllRequest;
+use Barsam\User\Messages\FieldUpdateRequest;
 use Barsam\User\Messages\GetRequest;
 use Barsam\User\Messages\RegisterRequest;
 use Barsam\User\Messages\UpdateRequest;
@@ -15,6 +19,10 @@ use Barsam\User\UserServiceInterface;
  * @method Delete(DeleteRequest $request, string $responseClass, array $context = [])
  * @method Get(GetRequest $request, string $responseClass, array $context = [])
  * @method CheckCredentials(RegisterRequest $request, string $responseClass, array $context = [])
+ * @method FieldCreate(FieldCreateRequest $request, string $responseClass, array $context = [])
+ * @method FieldUpdate(FieldUpdateRequest $request, string $responseClass, array $context = [])
+ * @method FieldDelete(FieldDeleteRequest $request, string $responseClass, array $context = [])
+ * @method FieldGetAll(FieldGetAllRequest $request, string $responseClass, array $context = [])
  */
 class UserService extends AbstractClient
 {
