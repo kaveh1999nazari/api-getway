f3317e76a9e83124b5a740abaa1f34db1cb9251d
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-16 10:49:05 +0330

    create installment gRPC Service

---
+++ app/src/Service/InstallmentService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Service;
+
+use App\Utility\GRPC\AbstractClient;
+use Barsam\Installment\InstallmentServiceInterface;
+use Barsam\Installment\Messages\CreateRequest;
+use Barsam\Installment\Messages\DeleteRequest;
+use Barsam\Installment\Messages\GenerateRequest;
+use Barsam\Installment\Messages\GetAllRequest;
+use Barsam\Installment\Messages\GetRequest;
+use Barsam\Installment\Messages\UpdateRequest;
+
+
+/**
+ * @method Create(CreateRequest $request, string $responseClass, array $context = [])
+ * @method Generate(GenerateRequest $request, string $responseClass, array $context = [])
+ * @method Update(UpdateRequest $request, string $responseClass, array $context = [])
+ * @method Delete(DeleteRequest $request, string $responseClass, array $context = [])
+ * @method GetAll(GetAllRequest $request, string $responseClass, array $context = [])
+ * @method Get(GetRequest $request, string $responseClass, array $context = [])
+ */
+class InstallmentService extends AbstractClient
+{
+
+    public function getServiceInterface(): string
+    {
+        return InstallmentServiceInterface::class;
+    }
+}
