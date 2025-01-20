75395ec01d39fcca4933c78cf7bd8df76c606b07
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-20 23:29:26 +0330

    auth

--- app/src/Endpoint/Web/AuthController.php
+++ app/src/Endpoint/Web/AuthController.php
@@ -2,136 +2,75 @@
 
 namespace App\Endpoint\Web;
 
-use App\Domain\Mapper\AuthorizeMapper;
-use App\Domain\Mapper\GenerateOtpMapper;
-use App\Domain\Mapper\GetSessionsMapper;
-use App\Domain\Mapper\IssueTokenMapper;
-use App\Domain\Mapper\LogoutSessionMapper;
-use App\Domain\Mapper\VerifyOtpMapper;
+
 use App\Service\AuthService;
+use App\Service\NotificationService;
+use App\Service\UserService;
+use App\Utility\GRPC\Response;
+use Barsam\Auth\Enums\ResponseType;
+use Barsam\Auth\Messages\AuthorizeRequest;
 use Barsam\Auth\Messages\AuthorizeResponse;
-use Barsam\Auth\Messages\GenerateOtpResponse;
-use Barsam\Auth\Messages\GetSessionsResponse;
-use Barsam\Auth\Messages\IssueTokenResponse;
-use Barsam\Auth\Messages\LogoutSessionResponse;
-use Barsam\Auth\Messages\VerifyOtpResponse;
+use Barsam\Notification\Messages\SendByTemplateRequest;
+use Barsam\Notification\Messages\SendByTemplateResponse;
+use Barsam\User\Messages\GetRequest;
+use Barsam\User\Messages\GetResponse;
+use Barsam\User\Models\User;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Spiral\Http\Exception\HttpException;
 use Spiral\Http\Request\InputManager;
 use Spiral\Router\Annotation\Route;
 
 class AuthController
 {
-    public function __construct(private readonly AuthService $authService)
+    public function __construct(
+        private readonly AuthService         $authService,
+        private readonly UserService         $userService,
+        private readonly NotificationService $notificationService,
+    )
     {
     }
 
-    #[Route('/api/', methods: ['POST'])]
-    public function authorize(ServerRequestInterface $request, InputManager $input): ResponseInterface
+    #[Route('/auth/request-otp', name: 'auth.request_otp', methods: ['POST'], group: 'api')]
+    public function requestOtp(InputManager $input): array
     {
-        $userRequest = AuthorizeMapper::fromRequest($input->data->all());
+        $getUserRequest = new GetRequest();
+        $getUserRequest->setLoginId($input->post('mobile'));
+        /** @var Response $usersResponse */
+        $usersResponse = $this->userService->Get(
+            $getUserRequest,
+            GetResponse::class
+        );
+        if (count($usersResponse->getResponse()->getUsers()) === 0) {
+            throw new HttpException('کاربر درخواستی یافت نشد.', 404);
+        }
 
-        $userResponse = $this->authService->Authorize(
-            $userRequest,
+        /** @var User $user */
+        $user = $usersResponse->getResponse()->getUsers()[0];
+
+        $authorizeRequest = new AuthorizeRequest();
+        $authorizeRequest->setClientId($user->getId());
+        $authorizeRequest->setResponseType(ResponseType::CODE);
+
+        /** @var Response $otpResponse */
+        $otpResponse = $this->authService->Authorize(
+            $authorizeRequest,
             AuthorizeResponse::class
         );
 
-        return $this->jsonResponse([
-            'code' => $userResponse->getResponse()->getCode(),
-            'state' => $userResponse->getResponse()->getState(),
-            'redirect_uri' => $userResponse->getResponse()->getRedirectUri(),
-        ]);
+        $otpCode = $otpResponse->getResponse()->getCode();
 
-    }
-
-    #[Route('/api/', methods: ['POST'])]
-    public function issueToken(ServerRequestInterface $request, InputManager $input): ResponseInterface
-    {
-        $userRequest = IssueTokenMapper::fromRequest($input->data->all());
-
-        $userResponse = $this->authService->IssueToken(
-            $userRequest,
-            IssueTokenResponse::class
+        $sendOtpRequest = new SendByTemplateRequest();
+        $sendOtpRequest->setRecipient($user->getLoginId());
+        $sendOtpRequest->setChannelId(1);
+        $sendOtpRequest->setData(['code' => $otpCode]);
+        $sendOtpRequest->setTemplate(1);
+        $otpResponse = $this->notificationService->SendByTemplate(
+            $sendOtpRequest,
+            SendByTemplateResponse::class
         );
 
-        return $this->jsonResponse([
-            'access_token' => $userResponse->getResponse()->getAccessToken(),
-            'token_type' => $userResponse->getResponse()->getTokenType(),
-            'expires_in' => $userResponse->getResponse()->getExpiresIn(),
-            'refresh_token' => $userResponse->getResponse()->getRefreshToken(),
-            'scope' => $userResponse->getResponse()->getScope(),
-            'user_id' => $userResponse->getResponse()->getUserId(),
-        ]);
-    }
-
-    #[Route('/api/', methods: ['POST'])]
-    public function getSessions(ServerRequestInterface $request, InputManager $input): ResponseInterface
-    {
-        $userRequest = GetSessionsMapper::fromRequest($input->data->all());
-
-        $userResponse = $this->authService->GetSessions(
-            $userRequest,
-            GetSessionsResponse::class
-        );
-
-        return $this->jsonResponse([
-            'sessions' => $userResponse->getResponse()->getSessions(),
-        ]);
-    }
-
-    #[Route('/api/', methods: ['POST'])]
-    public function logoutSession(ServerRequestInterface $request, InputManager $input): ResponseInterface
-    {
-        $userRequest = LogoutSessionMapper::fromRequest($input->data->all());
-
-        $userResponse = $this->authService->LogoutSession(
-            $userRequest,
-            LogoutSessionResponse::class
-        );
-
-        return $this->jsonResponse([
-            'message' => 'Successfully logged out',
-        ]);
-
-    }
-
-    #[Route('/api/', methods: ['POST'])]
-    public function generateOtp(ServerRequestInterface $request, InputManager $input): ResponseInterface
-    {
-        $userRequest = GenerateOtpMapper::fromRequest($input->data->all());
-
-        $userResponse = $this->authService->GenerateOtp(
-            $userRequest,
-            GenerateOtpResponse::class
-        );
-
-        return $this->jsonResponse([
-            'code' => $userResponse->getResponse()->getCode(),
-            'Qr_code_url' => $userResponse->getResponse()->getQrCodeUrl(),
-        ]);
-
-    }
-
-    #[Route('/api/', methods: ['POST'])]
-    public function verifyOtp(ServerRequestInterface $request, InputManager $input): ResponseInterface
-    {
-        $userRequest = VerifyOtpMapper::fromRequest($input->data->all());
-
-        $userResponse = $this->authService->VerifyOtp(
-            $userRequest,
-            VerifyOtpResponse::class
-        );
-
-        return $this->jsonResponse([
-            'valid' => $userResponse->getResponse()->getValid(),
-        ]);
-
-    }
-
-    private function jsonResponse(array $data, int $status = 200): ResponseInterface
-    {
-        $response = new \Nyholm\Psr7\Response($status);
-        $response->getBody()->write(json_encode($data));
-        return $response->withHeader('Content-Type', 'application/json');
+        dd($otpResponse);
+        return [];
     }
 }
