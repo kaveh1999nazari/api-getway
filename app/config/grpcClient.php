d892a265410ccb0e62177b515744d9a9f55c6fde
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-20 23:06:52 +0330

    update config file

--- app/config/grpcClient.php
+++ app/config/grpcClient.php
@@ -1,57 +1,62 @@
 <?php
+$serviceIp = '172.20.8.53';
 return [
     'services' => [
-        'user' => [
-            'class' => \App\Service\UserService::class,
-            'host' => '172.20.8.29:9001',
-        ],
-        'auth' => [
-            'class' => \App\Service\AuthService::class,
-            'host' => '172.20.8.29:9002',
-        ],
         'config' => [
             'class' => \App\Service\ConfigService::class,
-            'host' => '172.20.8.29:9003',
-        ],
-        'inquiry' => [
-            'class' => \App\Service\InquiryService::class,
-            'host' => '172.20.8.29:9004',
+            'host' => $serviceIp . ':50001',
         ],
         'installment' => [
             'class' => \App\Service\InstallmentService::class,
-            'host' => '172.20.8.29:9003'
+            'host' => $serviceIp . ':50002'
         ],
         'loan' => [
             'class' => \App\Service\LoanService::class,
-            'host' => '172.20.8.29:9004'
+            'host' => $serviceIp . ':50003'
         ],
         'notification' => [
             'class' => \App\Service\NotificationService::class,
-            'host' => '172.20.8.90:9005'
+            'host' => $serviceIp . ':50004'
         ],
         'ticket' => [
             'class' => \App\Service\TicketService::class,
-            'host' => '172.20.8.90:9008'
+            'host' => $serviceIp . ':50005'
+        ],
+        'user' => [
+            'class' => \App\Service\UserService::class,
+            'host' => $serviceIp . ':50006',
         ],
         'wallet' => [
             'class' => \App\Service\WalletService::class,
-            'host' => '172.20.8.90:9009'
-        ],
-        'cpg' => [
-            'class' => \App\Service\CpgService::class,
-            'host' => '172.20.8.90:90010'
+            'host' => $serviceIp . ':50007'
         ],
         'organization' => [
             'class' => \App\Service\OrganizationService::class,
-            'host' => '172.20.8.29:9002'
+            'host' => $serviceIp . ':50008'
         ],
-        'otp' => [
-            'class' => \App\Service\OtpService::class,
-            'host' => '172.20.8.90:90012'
+        'shop' => [
+            'class' => \App\Service\ShopService::class,
+            'host' => $serviceIp . ':50009'
+        ],
+        'auth' => [
+            'class' => \App\Service\AuthService::class,
+            'host' => $serviceIp . ':50010',
+        ],
+        'inquiry' => [
+            'class' => \App\Service\InquiryService::class,
+            'host' => $serviceIp . ':50011',
+        ],
+        'cpg' => [
+            'class' => \App\Service\CpgService::class,
+            'host' => $serviceIp . ':50012'
         ],
         'payment' => [
             'class' => \App\Service\PaymentService::class,
-            'host' => '172.20.8.90:90013'
-        ]
+            'host' => $serviceIp . ':50013'
+        ],
+//        'otp' => [
+//            'class' => \App\Service\OtpService::class,
+//            'host' => '172.20.8.90:90012'
+//        ],
     ]
 ];
