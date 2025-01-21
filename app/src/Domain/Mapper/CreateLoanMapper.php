a23204e99cfe0eb3c79899c5fb8a3e3f930b5901
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-21 22:32:37 +0330

    create CreateLoanMapper

--- app/src/Domain/Mapper/CreateLoanMapper.php
+++ app/src/Domain/Mapper/CreateLoanMapper.php
@@ -3,25 +3,16 @@
 namespace App\Domain\Mapper;
 
 use Barsam\Loan\Messages\CreateRequest;
+use Google\Protobuf\Timestamp;
 
 class CreateLoanMapper
 {
     public static function fromRequest(array $inputs): CreateRequest
     {
         return new CreateRequest([
-            'user_id' => $inputs['user_id'],
             'plan_id' => $inputs['plan_id'],
             'selected_amount' => $inputs['selected_amount'],
             'selected_duration' => $inputs['selected_duration'],
-            'installment_amount' => $inputs['installment_amount'],
-            'infrastructure_amount' => $inputs['infrastructure_amount'],
-            'prepayment_amount' => $inputs['prepayment_amount'],
-            'total_prepayment_amount' => $inputs['total_prepayment_amount'],
-            'total_user_payment' => $inputs['total_user_payment'],
-            'credit_remaining' => $inputs['credit_remaining'],
-            'granted_at' => $inputs['granted_at'],
-            'refund_bank_account_number' => $inputs['refund_bank_account_number'],
         ]);
     }
-
 }
