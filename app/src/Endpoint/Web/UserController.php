58ade494a4f8f37c1ed3a283e8a7e30a265054d8
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-21 22:35:26 +0330

    update UserController

--- app/src/Endpoint/Web/UserController.php
+++ app/src/Endpoint/Web/UserController.php
@@ -4,10 +4,13 @@ namespace App\Endpoint\Web;
 
 use App\Domain\Mapper\CreateUserMapper;
 use App\Service\AuthService;
+use App\Service\NotificationService;
 use App\Service\UserService;
 use Barsam\Auth\Enums\ResponseType;
 use Barsam\Auth\Messages\AuthorizeRequest;
 use Barsam\Auth\Messages\AuthorizeResponse;
+use Barsam\Notification\Messages\SendByTemplateRequest;
+use Barsam\Notification\Messages\SendByTemplateResponse;
 use Barsam\User\Messages\GetRequest;
 use Barsam\User\Messages\GetResponse;
 use Barsam\User\Messages\RegisterResponse;
@@ -23,51 +26,54 @@ class UserController
     public function __construct(
         private readonly UserService $userService,
         private readonly AuthService $authService,
+        private readonly NotificationService $notificationService,
     )
     {
     }
 
     #[Route('/auth/register', name: 'user.register', methods: ['POST'], group:'api')]
-    public function register(InputManager $input)
+    public function register(InputManager $input): array
     {
-        $userRequest = new GetRequest();
-        $userRequest->setLoginId($input->post('login_id'));
+        $registerRequest = CreateUserMapper::fromRequest($input->data->all());
 
         /** @var Response $registerResponse */
-        $userResponse = $this->userService->Get(
-            $userRequest,
-            GetResponse::class
-        );
-
-        if (count($userResponse->getResponse()->getUsers()) === 0)
-        {
-            $registerRequest = CreateUserMapper::fromRequest($input->data->all());
-
-            /** @var Response $registerResponse */
-            $registerResponse = $this->userService->Register(
+        $registerResponse = $this->userService->Register(
                 $registerRequest,
                 RegisterResponse::class
-            );
+        );
 
-            dump($registerResponse->getResponse());
 
-            $authorizeRequest = new AuthorizeRequest();
-            $authorizeRequest->setClientId($registerResponse->getResponse()->getUserId());
-            $authorizeRequest->setResponseType(ResponseType::CODE);
-
-            /** @var Response $otpResponse */
-            $otpResponse = $this->authService->Authorize(
-                $authorizeRequest,
-                AuthorizeResponse::class
-            );
-
-            $otpCode = $otpResponse->getResponse()->getCode();
-
-            dump($otpCode);
-
-        }else{
-            throw new HttpException('کاربر وجود دارد', 403);
+//        dd($registerResponse->getDetail());
+        if($registerResponse->getDetail()->code === 13)
+        {
+            throw new HttpException('این کاربر قبلا ثبت نام کرده است', 403);
         }
+
+
+        $authorizeRequest = new AuthorizeRequest();
+        $authorizeRequest->setClientId($registerResponse->getResponse()->getUserId());
+        $authorizeRequest->setResponseType(ResponseType::CODE);
+
+        /** @var Response $otpResponse */
+        $otpResponse = $this->authService->Authorize(
+            $authorizeRequest,
+            AuthorizeResponse::class
+        );
+
+        $otpCode = $otpResponse->getResponse()->getCode();
+
+        $sendOtpRequest = new SendByTemplateRequest();
+        $sendOtpRequest->setRecipient($input->post('login_id'));
+        $sendOtpRequest->setChannelId(2);
+        $sendOtpRequest->setData(['code' => $otpCode]);
+        $sendOtpRequest->setTemplate(2);
+        $otpResponse = $this->notificationService->SendByTemplate(
+            $sendOtpRequest,
+            SendByTemplateResponse::class
+        );
+
+
+        return [];
 
     }
 
