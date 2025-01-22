8c8aa9e7d1cda8db341d8ea58245291a6417a424
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-22 16:52:47 +0330

    create InstallmentController for list

--- app/src/Endpoint/Web/InstallmentController.php
+++ app/src/Endpoint/Web/InstallmentController.php
@@ -5,7 +5,10 @@ namespace App\Endpoint\Web;
 use App\Domain\Mapper\GetInstallmentMapper;
 use App\Facade\Auth;
 use App\Service\InstallmentService;
+use App\Utility\GoogleTimeHelper;
 use Barsam\Installment\Messages\GetResponse;
+use DateTimeInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Spiral\Http\Request\InputManager;
 use Spiral\Router\Annotation\Route;
 
@@ -17,20 +20,34 @@ class InstallmentController
     {
     }
 
-    #[Route('loans/:id/installments', name: 'loans.installments', methods: ['GET'], group: 'api_auth')]
-    public function list(InputManager $input, int $id): array
+    #[Route('loans/<id:\d+>/installments/', name: 'installments.list', methods: ['GET'], group: 'api_auth')]
+    public function list(InputManager $input, ServerRequestInterface $request): array
     {
-        $installmentRequest = GetInstallmentMapper::fromRequest($input->data->all(), $id, Auth::user()->getId());
+        $id = $request->getAttribute('route')->getMatches()['id'];
+        $installmentRequest = GetInstallmentMapper::fromRequest($input->query->all(), $id, Auth::user()->getId());
 
         $installmentResponse = $this->installmentService->Get(
             $installmentRequest,
             GetResponse::class
         );
 
-        dump($installmentResponse->getDetail());
-        dump($installmentResponse->getResponse());
+        $installments = $installmentResponse->getResponse()->getInstallments();
 
-        return [];
+        $installmentDetails = [];
+        foreach ($installments as $installment) {
+            $installmentDetails[] = [
+                'id' => $installment->getId(),
+                'user_id' => $installment->getUserId(),
+                'loan_id' => $installment->getLoanId(),
+                'number' => $installment->getNumber(),
+                'amount' => $installment->getAmount(),
+                'penalty_amount' => $installment->getPenaltyAmount(),
+                'created_at' => GoogleTimeHelper::timestampToDateTimeImmutable($installment->getCreatedAt())->format(DateTimeInterface::ATOM),
+                'updated_at' => GoogleTimeHelper::timestampToDateTimeImmutable($installment->getUpdatedAt())->format(DateTimeInterface::ATOM)
+            ];
+        }
+
+        return $installmentDetails;
+
     }
-
 }
