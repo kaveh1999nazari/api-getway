8ea32db47398643864ef2dab61960eb77b9a16ef
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-23 17:26:43 +0330

    update InstallmentController

--- app/src/Endpoint/Web/InstallmentController.php
+++ app/src/Endpoint/Web/InstallmentController.php
@@ -6,6 +6,7 @@ use App\Domain\Mapper\GetInstallmentMapper;
 use App\Facade\Auth;
 use App\Service\InstallmentService;
 use App\Utility\GoogleTimeHelper;
+use App\Utility\GRPC\Response;
 use Barsam\Installment\Messages\GetResponse;
 use DateTimeInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -26,11 +27,14 @@ class InstallmentController
         $id = $request->getAttribute('route')->getMatches()['id'];
         $installmentRequest = GetInstallmentMapper::fromRequest($input->query->all(), $id, Auth::user()->getId());
 
+        /** @var Response $installmentResponse */
         $installmentResponse = $this->installmentService->Get(
             $installmentRequest,
             GetResponse::class
         );
 
+//        dump($installmentResponse->getResponse());
+//        dump($installmentResponse->getDetail());
         $installments = $installmentResponse->getResponse()->getInstallments();
 
         $installmentDetails = [];
@@ -42,6 +46,7 @@ class InstallmentController
                 'number' => $installment->getNumber(),
                 'amount' => $installment->getAmount(),
                 'penalty_amount' => $installment->getPenaltyAmount(),
+                'due_date' => $installment->getDueDate(),
                 'created_at' => GoogleTimeHelper::timestampToDateTimeImmutable($installment->getCreatedAt())->format(DateTimeInterface::ATOM),
                 'updated_at' => GoogleTimeHelper::timestampToDateTimeImmutable($installment->getUpdatedAt())->format(DateTimeInterface::ATOM)
             ];
