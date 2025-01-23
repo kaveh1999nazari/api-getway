b9ad0bfa2e003f4946f57f0b0fec824060ec4501
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-01-23 22:36:53 +0330

    update WalletController.php

--- app/src/Endpoint/Web/WalletController.php
+++ app/src/Endpoint/Web/WalletController.php
@@ -5,6 +5,7 @@ namespace App\Endpoint\Web;
 use App\Domain\Mapper\Backup\WalletGetTransactionsMapper;
 use App\Service\WalletService;
 use App\Utility\GoogleTimeHelper;
+use App\Utility\GRPC\Response;
 use Barsam\Wallet\Messages\GetTransactionsResponse;
 use DateTimeInterface;
 use Spiral\Http\Request\InputManager;
@@ -23,32 +24,35 @@ class WalletController
     {
         $walletRequest = WalletGetTransactionsMapper::fromRequest($input->query->all());
 
+        /** @var Response<GetTransactionsResponse> $walletResponse */
         $walletResponse = $this->walletService->GetTransactions(
             $walletRequest,
             GetTransactionsResponse::class
         );
 
-        $transactions = $walletResponse->getResponse()->getTransactions();
-        $transactionsArray = [];
-        foreach ($transactions as $transaction) {
-            $transactionsArray[] = [
-                'id' => $transaction->getId(),
-                'wallet_id' => $transaction->getWalletId(),
-                'type' => $transaction->getType(),
-                'amount' => $transaction->getAmount(),
-                'description' => $transaction->getDescription(),
-                'status' => $transaction->getStatus(),
-                'caused_by_id' => $transaction->getCausedById(),
-                'caused_by_name' => $transaction->getCausedByName(),
-                'reference_number' => $transaction->getReferenceNumber(),
-                'created_at' => GoogleTimeHelper::timestampToDateTimeImmutable($transaction->getCreatedAt())->format(DateTimeInterface::ATOM),
-            ];
-        }
+        dump($walletResponse->getResponse());
+
+//        $transactions = $walletResponse->getResponse()->getTransactions();
+//        $transactionsArray = [];
+//        foreach ($transactions as $transaction) {
+//            $transactionsArray[] = [
+//                'id' => $transaction->getId(),
+//                'wallet_id' => $transaction->getWalletId(),
+//                'type' => $transaction->getType(),
+//                'amount' => $transaction->getAmount(),
+//                'description' => $transaction->getDescription(),
+//                'status' => $transaction->getStatus(),
+//                'caused_by_id' => $transaction->getCausedById(),
+//                'caused_by_name' => $transaction->getCausedByName(),
+//                'reference_number' => $transaction->getReferenceNumber(),
+//                'created_at' => GoogleTimeHelper::timestampToDateTimeImmutable($transaction->getCreatedAt())->format(DateTimeInterface::ATOM),
+//            ];
+//        }
 
         return [
-            'data' => $transactionsArray,
-            'max_page' => $walletResponse->getResponse()->getMaxPage(),
-            'total_records' => $walletResponse->getResponse()->getTotalRecords(),
+//            'data' => $transactionsArray,
+//            'max_page' => $walletResponse->getResponse()->getMaxPage(),
+//            'total_records' => $walletResponse->getResponse()->getTotalRecords(),
         ];
     }
 
